449a3675920e0b1ac1debbea587eeca90b707327
kiransamjhana <kiranmanandhar956@gmail.com>
2024-08-24 19:48:03 +1000

    new daisy UI added

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen background-color-blue flex-col items-center justify-between p-24">
       Hello Golden Supervision Academy Made by Kiran Manandhar
+      <button className="btn btn-primary">Hello daisyUI!</button>
     </main>
   );
 }
